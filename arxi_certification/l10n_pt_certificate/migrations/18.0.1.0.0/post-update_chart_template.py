f98a864722bc2af5ee86354a384b7eb0fcead951
Odoo Migration Engine <migrations@odoo-migrations.local>
2026-08-19 22:40:21 +0000

    auto-fix v19: filtra country_id.code em Python (campo deixou de ser stored no Odoo 19) em post-update_chart_template.py (iteração 17)

--- arxi_certification/l10n_pt_certificate/migrations/18.0.1.0.0/post-update_chart_template.py
+++ arxi_certification/l10n_pt_certificate/migrations/18.0.1.0.0/post-update_chart_template.py
@@ -5,6 +5,5 @@ def migrate(cr, version):
     """Update chart_template from 'pt_certificate' to 'pt_arxi' for Portuguese companies."""
     env = api.Environment(cr, SUPERUSER_ID, {})
     env['res.company'].search([
-        ('country_id.code', '=', 'PT'),
         ('chart_template', '=', 'pt_certificate')
-    ]).write({'chart_template': 'pt_arxi'})
+    ]).filtered(lambda c: c.country_id.code == 'PT').write({'chart_template': 'pt_arxi'})
